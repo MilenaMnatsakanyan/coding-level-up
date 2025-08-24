b9f1efec05321b2b782a29e356c817be20938899
MilenaMnatsakanyan <mnatsakanyanmilena87@gmail.com>
2025-08-24 21:35:04 +0400

    functon strpbrkk

---
+++ strpbrkk.c
@@ -0,0 +1,19 @@
+#include <stdio.h>  
+
+const char *strpbrkk(const char *str1, const char *str2) {
+    size_t i = 0;
+
+    while (str1[i] != '\0') {
+        size_t j = 0;
+        while (str2[j] != '\0') {
+            if (str1[i] == str2[j]) {
+                return &str1[i];  
+            }
+            j++;
+        }
+        i++;
+    }
+
+    return 0;  // եթե ոչինչ չգտնվեց
+}
+
