0b12acda54557ed0535aa04c4ec43e12997aff80
MilenaMnatsakanyan <mnatsakanyanmilena87@gmail.com>
2025-08-24 20:26:54 +0400

    Update main.c

--- main.c
+++ main.c
@@ -14,3 +14,16 @@ int main() {
 
     return 0;
 }
+
+#include "strspnn.h"
+
+int main() {
+    const char *str1 = "apple123";
+    const char *str2 = "apple";
+
+    size_t count1 = strspnn(str1, str2);
+
+    printf(" %zu\n", count1);
+
+    return 0;
+}
